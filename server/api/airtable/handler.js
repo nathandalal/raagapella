74b3fb007b1c4fef84d92eb85643671684bad9db
Nathan Dalal <nathanhdalal@gmail.com>
2017-04-02 22:08:18 -0300

    disabled slack notifs for a bit

--- server/api/airtable/handler.js
+++ server/api/airtable/handler.js
@@ -87,12 +87,12 @@ module.exports.registerAudition = (auditionid, name, email, references) => new P
 		console.log("Person added to airtable, now sending email confirmation and Slack confirmation message.")
 		let person = airtabledone[0].fields, audition = airtabledone[1].fields
 		return Promise.all([
-			SlackHandler.write(
-				`*${person['Name']}* (_${person["Email"]}_) just signed up for an audition!\n` + 
-				(person["Where did you hear about us?"] ? 
-					`They found out about us through ${person["Where did you hear about us?"].join(" and ")}.` :
-					"They didn't specify how they found out about us.")
-			),
+			// SlackHandler.write(
+			// 	`*${person['Name']}* (_${person["Email"]}_) just signed up for an audition!\n` + 
+			// 	(person["Where did you hear about us?"] ? 
+			// 		`They found out about us through ${person["Where did you hear about us?"].join(" and ")}.` :
+			// 		"They didn't specify how they found out about us.")
+			// ),
 			MailgunHandler.send(person, audition, 'audition')
 		])
 	})
